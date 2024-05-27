1d285a0ee537e732078c08c761e7fee59b40ff88
said2715374 <2715374@gmail.com>
2024-05-26 22:35:31 -0400

    relation entre les page

---
+++ Components/Accueil.jsx
@@ -0,0 +1,11 @@
+import styles from './Accueil.module.css';
+
+
+export default function Accueil() {
+    return <>
+        
+        <div className={styles.welcome}>
+            vien visiter bejaia
+        </div>
+    </>
+}
---
+++ Components/header.jsx
@@ -0,0 +1,26 @@
+import styles from './header.module.css';
+import Image from 'next/image';
+import logo from '../public/logo.webp'
+
+export default function Header({ setPage }) { // Ajout de la prop setPage
+    return (
+        <header className={styles.header}>
+            <Image
+               src={logo}
+               alt="Logo React"
+               width={80}
+            />
+            <nav className={styles.mainNav}> {/* Correction de la classe */}
+                <ul>
+                    <li><button onClick={() => setPage('accueil')}>Accueil</button></li>
+                    <li><button onClick={() => setPage('apropos')}>A propos</button></li>
+                    <li><button onClick={() => setPage('contact')}>Contact</button></li>
+                    <li><button onClick={() => setPage('reservation')}>Reservation</button></li>
+                    <li><button onClick={() => setPage('service')}>Service</button></li>
+                    <li><button onClick={() => setPage('tours')}>Tours</button></li>
+                </ul>
+            </nav>
+            <a href="#booking" className={styles.ctaButton}>Réservez maintenant</a>
+        </header>
+    );
+}
